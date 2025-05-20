315950e3fc338b37f74d49ce14c41b7d2fdd540b
Jorge Fabrizio Lopez Quiroz <giorgiolopez-@hotmail.com>
2025-05-20 15:28:16 +0200

    Update config.php
    Delete sensitive data

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Dati di connessione al database
-$host = '31.11.39.218';         // Host del database (se in locale, usi 'localhost')
-$dbname = 'Sql1862938_1'; // Nome del tuo database
-$username = 'Sql1862938';   // Il tuo username per il database
-$password = 'Fiogiokiko.20';   // La tua password per il database
+$host = '';
+$dbname = '';
+$username = '';
+$password = '';
 
 // Creazione della connessione
 try {
